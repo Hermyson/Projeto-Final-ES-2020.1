9387f3bda1d6e646fd4613ac114cce8dcc90b509
Hermyson <hermysoncassiano@gmail.com>
2021-07-19 12:38:15 -0300

    arquivos de configuração devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  root to:'home#index'
+  devise_for :users
+ 
+  resources :psicologos
+  resources :sessaos
+  resources :pacientes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
