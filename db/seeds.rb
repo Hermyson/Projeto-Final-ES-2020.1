b74ceff939348c695b5a28d23f7e72e751d5aa68
Hermyson <hermysoncassiano@gmail.com>
2021-07-19 14:12:25 -0300

    corrigindo problemas apontado pelo codacy em db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-u1 = User.create(email:'admin@admin.com', password:'123456',password_confirmation:'123456',roles_mask:'1')
-p1 = Psicologo.create(crp: '1234567',nome:'teste',user_id:1)
+User.create(email:'admin@admin.com', password:'123456',password_confirmation:'123456',roles_mask:'1')
+Psicologo.create(crp: '1234567',nome:'teste',user_id:1)
